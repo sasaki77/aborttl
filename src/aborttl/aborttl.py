a591aca694d89badad9832c1b5351df7a7c8240d
Shinya Sasaki <shinya.sasaki@kek.jp>
2019-04-17 10:50:23 +0900

    Add aborttl module

---
+++ src/aborttl/aborttl.py
@@ -0,0 +1,183 @@
+import threading
+import time
+from queue import Queue
+
+from .logger import get_default_logger
+from .dbhandler import DbHandler
+from .abortch import AbortCh
+from .resetpvcounter import ResetPVCounter
+
+
+class AbortInfo(object):
+    def __init__(self):
+        self.id = None
+        self.sec = None
+        self.ts = None
+        self.reset_offset = 0
+
+    def clear(self):
+        self.id = None
+        self.sec = None
+        self.ts = None
+        self.reset_offset = 0
+
+
+class Aborttl(object):
+    '''
+    _pvs = {'pvname': {'msg': , 'abortch':, 'ring': }}
+    _abt_statuses: dict of pvname. False = ready, True = abort
+    '''
+
+    def __init__(self, dburi, resetpvname, logger=None):
+        self._logger = logger or get_default_logger()
+        self._resetpvname = resetpvname
+
+        self._dh = DbHandler(dburi)
+        self._pvs = {}
+
+        self._abt_q = Queue()
+        self._aborts = {'LER': {}, 'HER': {}}
+        self._abtinfo = {'LER': AbortInfo(),
+                         'HER': AbortInfo()}
+
+        self.__is_stop = threading.Event()
+        self.__stop_request = False
+        self._is_running = False
+
+        self._interval = 0.1
+        self._both_ring_interval = 5
+
+        self._init_pv()
+
+    def _init_pv(self):
+        self._resetpv = ResetPVCounter(self._resetpvname)
+
+        self._update_pvlist()
+
+        for pvname, item in self._pvs.items():
+            item['abortch'] = AbortCh(pvname, self._cb)
+
+    def _cb(self, pvname=None, value=None):
+        self._logger.debug('Put {}, {} to queue'.format(pvname, bool(value)))
+        self._abt_q.put((pvname, bool(value)))
+
+    def _update_pvlist(self):
+        pvs = self._dh.fetch_current_pvs()
+        self._pvs = {pv['pvname']: {'ring': pv['ring'], 'msg': pv['msg']}
+                     for pv in pvs}
+
+    def _run_loop(self):
+        qsize = self._abt_q.qsize()
+        if qsize:
+            self._logger.debug('No. of update ch = {}'.format(qsize))
+
+        for i in range(qsize):
+            pvname, abort = self._abt_q.get_nowait()
+            ring = self._pvs[pvname]['ring']
+            abtinfo = self._abtinfo[ring]
+
+            self._logger.debug('Update {} abort = {}, ring = {}'
+                               .format(pvname, abort, ring))
+
+            # update abort status
+            self._aborts[ring][pvname] = abort
+
+            if not abort:
+                continue
+
+            ch = self._pvs[pvname]['abortch']
+            timestamp = ch.get_timestamp()
+
+            # timestamp is not updated yet
+            if timestamp == "1970-01-01 09:00:00.000000000":
+                self._abt_q.put((pvname, abort))
+                self._logger.debug('{} timestamp is not updated'
+                                   .format(pvname))
+                continue
+
+            # new abort is comming
+            if abtinfo.id is None:
+
+                # Check the abort time can be treated as identical abort
+                rring = 'HER' if ring == 'LER' else 'LER'
+                rabtinfo = self._abtinfo[rring]
+                if (
+                      rabtinfo.id is not None and
+                      abs(ch.ts_sec - rabtinfo.sec) < 5
+                   ):
+
+                    # Both ring abort
+                    self._logger.debug('Both ring abort')
+
+                    abtinfo.id = rabtinfo.id
+                    abtinfo.sec = rabtinfo.sec
+                    abtinfo.ts = rabtinfo.ts
+                    abtinfo.reset_offset = rabtinfo.reset_offset
+                else:
+                    # Single ring abort
+                    self._logger.debug('Single ring abort')
+
+                    abtinfo.id = self._dh.insert_abort(timestamp)
+                    abtinfo.sec = ch.ts_sec
+                    abtinfo.ts = timestamp
+                    abtinfo.reset_offset = self._resetpv.count
+
+            # new faster abort is comming
+            if timestamp < abtinfo.ts:
+                self._logger.debug('New faster abort is comming')
+                self._dh.update_abort(abtinfo.id, timestamp)
+                abtinfo.sec = ch.ts_sec
+                abtinfo.ts = timestamp
+
+            # insert abort signal
+            msg = self._pvs[pvname]['msg']
+            ts = ch.ts
+            abtid = abtinfo.id
+
+            signal = {'pvname': pvname, 'msg': msg, 'pv_ts': ts,
+                      'abt_ts': timestamp,
+                      'reset_cnt': self._resetpv.count - abtinfo.reset_offset,
+                      'trg_cnt': ch.tcnt, 'int_cnt': ch.acnt}
+
+            self._logger.debug('Insert abot signal: {}'.format(pvname))
+            self._dh.insert_abort_signals([signal], abtid)
+
+        # check abort status for each ring
+        if qsize:
+            self._update_ring_status()
+
+    def _update_ring_status(self):
+        all_aborts = 0
+        rings = ['LER', 'HER']
+
+        for ring in rings:
+            aborts = self._aborts[ring]
+            is_abort = sum(aborts.values())
+            all_aborts += is_abort
+
+            if not is_abort:
+                self._abtinfo[ring].clear()
+                self._logger.debug('Clear {} Abort Info'.format(ring))
+
+        if not all_aborts:
+            self._resetpv.clear()
+            self._logger.debug('Clear Reset PV Counter')
+
+    def run(self):
+        self._is_running = True
+        self.__is_stop.clear()
+
+        try:
+            while not self.__stop_request:
+                self._run_loop()
+                time.sleep(self._interval)
+        finally:
+            self._logger.info('Aborttl stopped.')
+            self.__stop_request = False
+            self.__is_stop.set()
+
+        self._is_running = False
+
+    def stop(self):
+        self.__stop_request = True
+        self.__is_stop.wait()
---
+++ tests/test_aborttl.py
@@ -0,0 +1,245 @@
+import threading
+import time
+from datetime import datetime
+from collections import namedtuple
+
+import pytest
+from epics import PV
+from epics.ca import CAThread
+
+from aborttl.dbhandler import DbHandler
+from aborttl.abortch import AbortCh
+from aborttl.aborttl import Aborttl
+
+
+Signal = namedtuple('Signal', ['abt_id', 'ts', 'pvname', 'msg',
+                               'ring', 'reset', 'tcnt', 'acnt'
+                               ]
+                    )
+
+
+def insert_current_pv_mock(uri):
+    dh = DbHandler(uri)
+    conn = dh.engine.connect()
+
+    mock_pvs = [
+                   {'pvname': 'ET_dummyHost:ABORTCH1', 'ring': 'HER'},
+                   {'pvname': 'ET_dummyHost:ABORTCH2', 'ring': 'HER'},
+                   {'pvname': 'ET_dummyHost:ABORTCH3', 'ring': 'HER'},
+                   {'pvname': 'ET_dummyHost:ABORTCH4', 'ring': 'LER'},
+                   {'pvname': 'ET_dummyHost:ABORTCH5', 'ring': 'LER'}
+                ]
+
+    mock_cpvs = [
+                    {'pvname': 'ET_dummyHost:ABORTCH1', 'msg': 'msg 1'},
+                    {'pvname': 'ET_dummyHost:ABORTCH2', 'msg': 'msg 2'},
+                    {'pvname': 'ET_dummyHost:ABORTCH3', 'msg': 'msg 3'},
+                    {'pvname': 'ET_dummyHost:ABORTCH4', 'msg': 'msg 4'},
+                    {'pvname': 'ET_dummyHost:ABORTCH5', 'msg': 'msg 5'}
+                 ]
+
+    conn.execute(dh.tables['pvs'].insert(), mock_pvs)
+    conn.execute(dh.tables['current_pvs'].insert(), mock_cpvs)
+
+    conn.close()
+
+    return dh
+
+
+def clear_ch(ch_num):
+    name = "ET_dummyHost:ABORTCH" + str(ch_num)
+
+    pv_abort = PV(name)
+    pv_acnt = PV(name + ':ACNT')
+    pv_tcnt = PV(name + ':TCNT')
+    pv_sec = PV(name + ':TIME_SEC')
+    pv_nsec = PV(name + ':TIME_NANO')
+
+    pv_abort.put(0,  wait=True)
+    pv_acnt.put(0,  wait=True)
+    pv_tcnt.put(0,  wait=True)
+    pv_sec.put(0,  wait=True)
+    pv_nsec.put(0,  wait=True)
+
+
+def put_abort_ch(ch_num, acnt, tcnt, _t=None):
+    t = time.time() if _t is None else _t
+    name = "ET_dummyHost:ABORTCH" + str(ch_num)
+    t_sec, t_nano = ("%.9f" % t).split(".")
+
+    pv_abort = PV(name)
+    pv_acnt = PV(name + ':ACNT')
+    pv_tcnt = PV(name + ':TCNT')
+    pv_sec = PV(name + ':TIME_SEC')
+    pv_nsec = PV(name + ':TIME_NANO')
+
+    pv_abort.put(1,  wait=True)
+    pv_acnt.put(acnt,  wait=True)
+    pv_tcnt.put(tcnt,  wait=True)
+    pv_sec.put(int(t_sec),  wait=True)
+    pv_nsec.put(int(t_nano),  wait=True)
+
+    dt = datetime.fromtimestamp(int(t_sec))
+    return '{}.{}'.format(dt.isoformat(' '), t_nano)
+
+
+def abort_reset():
+    pv = PV('ET_dummyHost:RESETw')
+    pv.put(1,  wait=True)
+    time.sleep(1.1)
+
+
+def check_signals(ss_db, ss_test):
+    for s_db, s_test in zip(ss_db, ss_test):
+        err_msg = '{} = {}'.format(s_db, s_test)
+        assert s_db['abt_id'] == s_test.abt_id, err_msg
+        assert s_db['ts'] == s_test.ts, err_msg
+        assert s_db['pvname'] == s_test.pvname, err_msg
+        assert s_db['msg'] == s_test.msg, err_msg
+        assert s_db['ring'] == s_test.ring, err_msg
+        assert s_db['reset_cnt'] == s_test.reset, err_msg
+        assert s_db['trg_cnt'] == s_test.tcnt, err_msg
+        assert s_db['int_cnt'] == s_test.acnt, err_msg
+
+
+@pytest.fixture(scope='module')
+def atl(caclient, tmpdir_factory):
+    AbortCh.fields['ACNT'] = ':ACNT'
+    AbortCh.fields['TCNT'] = ':TCNT'
+    dburi = 'sqlite:///' + str(tmpdir_factory.mktemp('data').join('testdata.db'))
+    insert_current_pv_mock(dburi)
+    atl = Aborttl(dburi, 'ET_dummyHost:RESETw')
+
+    thread = CAThread(target=atl.run)
+    thread.daemon = True
+    thread.start()
+    time.sleep(5)
+    yield atl
+    atl.stop()
+    thread.join()
+    time.sleep(1)
+
+
+def test_single_ring_abort(softioc, caclient, atl):
+    t1 = put_abort_ch(1, 0, 0)
+    t2 = put_abort_ch(2, 1, 0)
+
+    time.sleep(2)
+    abort_reset()
+    clear_ch(1)
+    time.sleep(6)
+
+    put_abort_ch(1, 0, 1)
+    t3 = put_abort_ch(3, 1, 1)
+    t4 = put_abort_ch(4, 2, 1)
+    put_abort_ch(1, 1, 1)
+    t5 = put_abort_ch(5, 1, 2)
+
+    time.sleep(2)
+
+    signals = atl._dh.fetch_abort_signals()
+
+    ss = [Signal(1, t1, 'ET_dummyHost:ABORTCH1', 'msg 1', 'HER', 0, 0, 0),
+          Signal(1, t2, 'ET_dummyHost:ABORTCH2', 'msg 2', 'HER', 0, 0, 1),
+          Signal(1, t3, 'ET_dummyHost:ABORTCH3', 'msg 3', 'HER', 1, 1, 1),
+          Signal(2, t4, 'ET_dummyHost:ABORTCH4', 'msg 4', 'LER', 0, 1, 2),
+          Signal(2, t5, 'ET_dummyHost:ABORTCH5', 'msg 5', 'LER', 0, 2, 1)]
+
+    check_signals(signals, ss)
+
+    for i in range(5):
+        clear_ch(i+1)
+    time.sleep(1)
+
+
+def test_both_ring_abort(softioc, caclient, atl):
+    init_time = time.time()
+    init_dt = datetime.fromtimestamp(init_time)
+
+    time.sleep(1)
+
+    t1 = put_abort_ch(1, 0, 0)
+    t2 = put_abort_ch(2, 1, 0)
+
+    time.sleep(1)
+    abort_reset()
+    clear_ch(1)
+    time.sleep(1)
+
+    put_abort_ch(1, 0, 1)
+    t3 = put_abort_ch(3, 1, 1)
+    t4 = put_abort_ch(4, 2, 1)
+    put_abort_ch(1, 1, 1)
+    t5 = put_abort_ch(5, 1, 2)
+
+    time.sleep(2)
+
+    signals = atl._dh.fetch_abort_signals(astart=init_dt.isoformat(' '))
+
+    ss = [Signal(3, t1, 'ET_dummyHost:ABORTCH1', 'msg 1', 'HER', 0, 0, 0),
+          Signal(3, t2, 'ET_dummyHost:ABORTCH2', 'msg 2', 'HER', 0, 0, 1),
+          Signal(3, t3, 'ET_dummyHost:ABORTCH3', 'msg 3', 'HER', 1, 1, 1),
+          Signal(3, t4, 'ET_dummyHost:ABORTCH4', 'msg 4', 'LER', 1, 1, 2),
+          Signal(3, t5, 'ET_dummyHost:ABORTCH5', 'msg 5', 'LER', 1, 2, 1)]
+
+    check_signals(signals, ss)
+
+    for i in range(5):
+        clear_ch(i+1)
+    time.sleep(1)
+
+
+def test_new_faster_abort(softioc, caclient, atl):
+    init_time = time.time()
+    init_dt = datetime.fromtimestamp(init_time)
+
+    time.sleep(1)
+    t1_time = time.time()
+    time.sleep(1)
+    mid_time = time.time()
+    mid_dt = datetime.fromtimestamp(mid_time)
+    time.sleep(1)
+
+    t2 = put_abort_ch(2, 0, 1)
+    time.sleep(1)
+    t1 = put_abort_ch(1, 0, 0, t1_time)
+    time.sleep(1)
+    t3 = put_abort_ch(3, 1, 1)
+
+    time.sleep(2)
+
+    signals = atl._dh.fetch_abort_signals(astart=mid_dt.isoformat(' '))
+    assert signals == []
+
+    signals = atl._dh.fetch_abort_signals(astart=init_dt.isoformat(' '))
+
+    ss = [Signal(4, t1, 'ET_dummyHost:ABORTCH1', 'msg 1', 'HER', 0, 0, 0),
+          Signal(4, t2, 'ET_dummyHost:ABORTCH2', 'msg 2', 'HER', 0, 1, 0),
+          Signal(4, t3, 'ET_dummyHost:ABORTCH3', 'msg 3', 'HER', 0, 1, 1)]
+
+    check_signals(signals, ss)
+
+    for i in range(5):
+        clear_ch(i+1)
+    time.sleep(1)
+
+
+def test_timestamp_update_later(softioc, caclient, atl):
+    init_time = time.time()
+    init_dt = datetime.fromtimestamp(init_time)
+
+    put_abort_ch(1, 0, 0, 0)
+    time.sleep(1)
+    clear_ch(1)
+    t1 = put_abort_ch(1, 0, 0)
+
+    time.sleep(2)
+
+    signals = atl._dh.fetch_abort_signals(astart=init_dt.isoformat(' '))
+
+    ss = [Signal(5, t1, 'ET_dummyHost:ABORTCH1', 'msg 1', 'HER', 0, 0, 0)]
+
+    check_signals(signals, ss)
+
+    for i in range(5):
+        clear_ch(i+1)
