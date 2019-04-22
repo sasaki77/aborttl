85d33e1f4f081726a460c7e50e83c914f54d1797
Shinya Sasaki <shinya.sasaki@kek.jp>
2019-04-22 14:57:33 +0900

    Fix logger settings in main

--- src/aborttl/main.py
+++ src/aborttl/main.py
@@ -64,8 +64,9 @@ def main():
     args = parse_args()
 
     if args.logconfig:
-        logger = logging.config.fileConfig(args.logconfig,
-                                           disable_existing_loggers=False)
+        logging.config.fileConfig(args.logconfig,
+                                  disable_existing_loggers=False)
+        logger = logging.getLogger(__name__)
     else:
         logger = get_default_logger()
 
